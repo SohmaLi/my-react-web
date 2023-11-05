0e4150c4cb8bad1a3707e65653384be5ab90b1aa
SohmaLi <linhtruong6404@gmail.com>
2023-11-05 16:10:12 +0700

    Set up web reactjs

---
+++ src/pages/users/homepage/index.js
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+const HomePage = () => {
+    return (
+        <>
+            <h1>Hell</h1>
+            <h1>Hell1</h1>
+            <h1>Hell2</h1>
+        </>
+    );
+}
+export default memo(HomePage);
