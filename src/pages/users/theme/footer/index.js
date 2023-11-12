168329e273e47c702a808d0518b1d0ff50c732b2
SohmaLi <linhtruong6404@gmail.com>
2023-11-12 12:47:22 +0700

    add style for footer header

--- src/pages/users/theme/footer/index.js
+++ src/pages/users/theme/footer/index.js
@@ -2,9 +2,9 @@ import { memo } from "react";
 import "./style.scss";
 const Footer = () => {
   return (
-    <>
+    <div className="footer">
       <h1>Hell32</h1>
-    </>
+    </div>
   );
 };
 export default memo(Footer);
